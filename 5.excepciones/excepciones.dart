2ccfba258d4072a11d8cf7cf24f94b0c0a8d9814
pablo-herrera-celis <pablo.herrera.celis@unillanos.edu.co>
2022-09-03 16:00:07 -0500

    Excepciones
    Los tipos de excepciones que existen

--- 5.excepciones/excepciones.dart
+++ 5.excepciones/excepciones.dart
@@ -1,1 +1,24 @@
-void main() {}
+void main() {
+  try {
+    String elNombre = obtenerNombre("german");
+    print(elNombre);
+  } on FormatException catch (e) {
+    print("Se presento una excepcion FormatException = $e");
+  } on Exception catch (e) {
+    print("Se presenta una exception = $e");
+  } catch (e) {
+    print("Se presento una excepcion");
+  } finally {
+    print("Este es el finally");
+  }
+}
+
+String obtenerNombre(String nombre) {
+  if (nombre == 'german') {
+    throw FormatException("Este nombre no esta permitido");
+  }
+  if (nombre == 'Miguel') {
+    throw Exception("Este nombre no esta permitido");
+  }
+  return "Mi nombre es $nombre";
+}
