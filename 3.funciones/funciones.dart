fdf5254b3c051aa5924d51be30c828da2ea2ecd2
pablo-herrera-celis <pablo.herrera.celis@unillanos.edu.co>
2022-09-03 11:52:43 -0500

    Funciones
    Tipos de funciones que se pueden usar.

--- 3.funciones/funciones.dart
+++ 3.funciones/funciones.dart
@@ -1,1 +1,25 @@
-void main() {}
+void main() {
+  print(obtenerNombre("Juan"));
+  print(obtenerApellido("Juan","Herrera"));
+  print(obtenerApellidoNameParameter(apellido: "Celis"));
+
+}
+String obtenerNombre(String valor){
+  return 'Mi nombre es $valor';
+}
+
+String obtenerApellido(String nombre, String apellido){
+  return 'Mi nombre es $nombre $apellido';
+}
+
+String obtenerApellidoNameParameter({String? nombre, required String apellido, int edad =25}){
+  return 'Mi nombre es $nombre $apellido';
+}
+
+String obtenerApellidoDos(String nombre, [String? apellido]){
+  return 'Mi nombre es $nombre $apellido';
+}
+
+var miFuncionAnonima = (String valor){
+  return "El valor es $valor";
+};
